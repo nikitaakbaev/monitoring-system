d90141fb53eb4a4245b69f2a418269ff0e51e701
chery <nikitaakbaev050@gmail.com>
2025-06-05 21:02:51 +0400

    Fixing a bug with the userPassChange() function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,20 +11,25 @@ class UserController extends Controller
 {
     public function authUser(Request $request) {
         $fillable = $request->validate([
-           'email' => 'required|string|email|max:255',
-           'password' => 'required|string|min:3|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:3|max:255',
         ]);
 
-        if (Auth::attempt($fillable, $request -> remember) && Auth::user() -> is_active == 0) {
-            return redirect() -> route('passChange');
-        } elseif (Auth::user() -> is_active == 1) {
-            return redirect() -> route('home');
-        } else {
-            return back() -> withErrors([
-                'failAuth' => 'Ошибка в почте или пароле.'
-            ]) -> withInput();
-        }
+        // Пытаемся залогиниться
+        if (Auth::attempt($fillable, $request->remember)) {
+            $user = Auth::user();
 
+            if ($user->is_active == 0) {
+                return redirect()->route('passChange');
+            } else {
+                return redirect()->route('home');
+            }
+        } else {
+
+            return back()->withErrors([
+                'failAuth' => 'Ошибка в почте или пароле.'
+            ])->withInput();
+        }
     }
 
     public function logout() {
